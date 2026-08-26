baf638af54e40bc6f2967d810b17b5a312fbdf92
victoria <v.suprun1961@gmail.com>
2026-08-26 04:34:40 +0300

    feat: add a function that displays a list of car names from the database on the screen

--- src/ui/garage.ts
+++ src/ui/garage.ts
@@ -1,4 +1,5 @@
 import { getState } from "../state/store";
+import type { Car } from "../types/car";
 
 export function renderGarage(): HTMLElement {
   const state = getState();
@@ -6,6 +7,19 @@ export function renderGarage(): HTMLElement {
   const container = document.createElement("div");
     const title = document.createElement("h2");
     title.textContent = `Гараж (${state.garagePagination.currentPage} из ${state.garagePagination.totalElements})`;
-    container.appendChild(title);
+  container.appendChild(title);
+
+  state.cars.forEach((car) => {
+    const carElement = renderCarItem(car);
+    container.appendChild(carElement);
+  });
   return container;
 }
+
+
+function renderCarItem(car: Car): HTMLElement {
+  const item = document.createElement("div");
+  item.textContent = car.name;
+  return item;
+}
+
